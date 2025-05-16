36e678dd71ffe3d6417481e77b6d898b68d47ab4
Samuelux979 <holaguenas1@gmail.com>
2025-05-16 17:20:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hola Puro Juan! 🚀 (versión: {os.getenv('VERSION', '1.0')})"
+    return f"Hola Me encanta Antonio! 🚀 (versión: {os.getenv('VERSION', '1.0')})"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
